6922b6df22386b7ebfc5eaa619ea4cc72ac96ef9
hellopsm <psm2000163@163.com>
2023-03-08 19:44:15 +0800

    Update Button.vue

--- src/components/common/HoverButton/Button.vue
+++ src/components/common/HoverButton/Button.vue
@@ -1,4 +1,5 @@
 <script setup lang='ts'>
+/**
 interface Emit {
   (e: 'click'): void
 }
@@ -8,13 +9,9 @@ const emit = defineEmits<Emit>()
 function handleClick() {
   emit('click')
 }
+**/
 </script>
 
 <template>
-  <button
-    class="flex items-center justify-center w-10 h-10 transition rounded-full hover:bg-neutral-100 dark:hover:bg-[#414755]"
-    @click="handleClick"
-  >
-    <slot />
-  </button>
+
 </template>
